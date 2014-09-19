1c596eb6ec745fb90d05153c183eb48c14f87206
Eric <erjohnson.pdx@gmail.com>
2014-09-19 14:31:00 -0700

    update app root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "locations#index"
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: { registrations: "users/registrations" }
   resources :locations
   resources :messages
