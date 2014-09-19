7cbb20c02df4753533c292065387b987e75859c7
Eric <erjohnson.pdx@gmail.com>
2014-09-19 12:14:35 -0700

    update model relations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ActiveRecord::Base
+  has_many :messages
+
+  validates_presence_of :name
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ActiveRecord::Base
+  validates_presence_of :content
+  belongs_to :user, :location
 end
